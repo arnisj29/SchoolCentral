96457f69a6879d6c785ef5d583edb3404e75fab3
Arnis Juknevicius <arnis.juknevicius@icloud.com>
2025-09-01 20:45:02 -0400

    Initial commit

---
+++ SchoolCentral/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolCentral
+//
+//  Created by Arnis Juknevicius on 9/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolCentral/SchoolCentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolCentralApp.swift
+//  SchoolCentral
+//
+//  Created by Arnis Juknevicius on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolCentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
